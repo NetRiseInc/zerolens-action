8e92793f1eb10db7c3887c62b9708882805bb650
Michael Scott <michael.scott@netrise.io>
2025-04-27 17:20:35 -0500

    updates dogfood check for CWE-676

--- examples/dummy-project/src/main.c
+++ examples/dummy-project/src/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include <string.h>   // CWE-120 (unsafe strcpy)
+#include <string.h> // CWE-120 (unsafe strcpy) and CWE-676 (dangerous strcat)
 
-int main(void) {
+int main(void)
+{
     char buf[4];
-    strcpy(buf, "OVERFLOW");   // CWE-120 / CWE-119
-    printf("hello world – %s\n", buf);
+    strcpy(buf, "OVERFLOW"); // CWE-120 / CWE-119
+
+    char msg[32] = "Hello ";
+    strcat(msg, buf); // CWE-676 – dangerous strcat
+
+    printf("hello world – %s\n", msg);
     return 0;
 }
